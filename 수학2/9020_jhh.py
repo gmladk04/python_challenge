d2f692b397ecbce31e69ab555c0ed0e9d5b1062b
JEONHyeonHui <gmladk04>
2020-10-15 19:11:17 +0900

    20201015
    수학2 드디어 다풀었노,,,,,,,골드바흐 나쁜 좌식아

--- 수학2/9020_jhh.py
+++ 수학2/9020_jhh.py
@@ -1,7 +1,21 @@
-#소수 리스트 생성
-#리스트 사용해서 연산?
-import math
+#n이하의 숫자들 중 소수 찾기
+def prime(n):
+    a=[True]*n
+    m=int(n**0.5)
+    for i in range(2,m+1):
+        if a[i]==True:
+            for j in range(i*i,n,i):
+                a[j]=False
+    return [i for i in range(2,n) if a[i] == True]
 
-n=int(input())
-for _ in range(n):
-    num=int(input())
+def find(n):
+    l=prime(n)
+    index=max([i for i in range(len(l)) if l[i]<=n/2])
+    for i in range(index,-1,-1):
+        for j in range(i, len(l)):
+            if l[i]+l[j]==n:
+                return [l[i],l[j]]
+
+for _ in range(int(input())):
+    n=int(input())
+    print(" ".join(map(str,find(n))))
