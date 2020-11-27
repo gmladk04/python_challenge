384736d9f8a2745294bb5d6de22b19f62bc57374
JEONHyeonHui <gmladk04>
2020-11-27 20:52:50 +0900

    201127
    DP 조졌따

---
+++ 동적 계획법1/12865_jhh.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int d[100001];
+int w[101];
+int v[101];
+int main(){
+    int n,k;
+    cin>>n>>k;
+    for(int i=1;i<=n;i++){
+        cin>>w[i]>>v[i];
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=k;j>=1;j--){
+            if(j-w[i]>=0)
+                d[j]=max(d[j],d[j-w[i]]+v[i]);
+        }
+    }
+    cout<<d[k]<<'\n';
+    return 0;
+}
