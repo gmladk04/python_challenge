82f33a228e1b4a82120a8a8c647b3b550b9c826b
JEONHyeonHui <gmladk08@gmail.com>
2020-12-04 14:27:47 +0900

    201204
    코드짜깁기,,,,,,,하는거 아니다,,,,,,,,디버깅 지옥이었따,,,,,,,,,ㅠ

--- 문자열 알고리즘1/1786_jhh.cpp
+++ 문자열 알고리즘1/1786_jhh.cpp
@@ -8,12 +8,15 @@ vector<int> preprocessing(string p) {
 	pi[0] = 0;
 	int j = 0;
 	for (int i = 1; i < m; i++) {
-		while (j > 0 && p[i] != p[j]) j = pi[j - 1];
+		while (j > 0 && p[i] != p[j])
+			j = pi[j - 1];
 		if (p[i] == p[j]) {
-			p[i] = j + 1;
+			pi[i] = j + 1;
 			j += 1;
 		}
-		else p[i] = 0;
+		else {
+			pi[i] = 0;
+		}
 	}
 	return pi;
 }
@@ -25,13 +28,17 @@ vector<int> kmp(string s, string p) {
 	int i = 0;
 	int j = 0;
 	for (int i = 0; i < n; i++) {
-		while (j > 0 && s[i] != p[j]) j = pi[j - 1];
+		while (j > 0 && s[i] != p[j]) {
+			j = pi[j - 1];
+		}
 		if (s[i] == p[j]) {
 			if (j == m - 1) {
 				ans.push_back(i - m + 1);
 				j = pi[j];
 			}
-			else j += 1;
+			else {
+				j += 1;
+			}
 		}
 	}
 	return ans;
@@ -42,8 +49,9 @@ int main() {
 	getline(cin, p);
 	auto matched = kmp(s, p);
 	cout << matched.size() << '\n';
-	for (auto index : matched)
+	for (auto index : matched) {
 		cout << index + 1 << ' ';
+	}
 	cout << '\n';
 	return 0;
 }
